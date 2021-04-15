9f89e1bceac0bc0c6ee1f22987b5c4194de3e669
jumaber <jumaber@gmail.com>
2021-04-15 17:04:33 +0200

    Added my first JS line!

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+console.log("Hello Anil!");
+
+
+/* I know this is not in the task, and I don't know how to reference yet, but I thought it would be funny :) 
+
+var Julia = nice;
+var Anil = Julia; // :)
+
+*/
