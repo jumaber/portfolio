f7f26b81336a6e275e0748eaff92099887e03ce4
jumaber <jumaber@gmail.com>
2021-04-19 14:33:34 +0200

    Improved animations and Project Section on Index

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 console.log("Hello Anil!");
 
+var text1 = "Julia's portfolio is \"amazing\"!";
+console.log(text1);
 
 /* I know this is not in the task, and I don't know how to reference yet, but I thought it would be funny :) 
 
